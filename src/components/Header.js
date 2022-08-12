e37bf0a3cfd44c38ec386969a6abef07bf9afdf1
Takuya Toyokawa <takuya.k.toyokawa@protonmail.com>
2022-08-12 10:55:23 -0700

    Updated based on Netlify error

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(){
     return (
         <header className="header">
-            <img src="../images/logo.png" />
+            <img src="../images/logo.png" alt="Logo of mytraveljournal" />
             <p>my travel journal.</p>
         </header>
     )
